c25f152bd7844dfa106ba09efc7c3889edbd5b23
ByeonggeunSon <qudrms007@naver.com>
2020-02-06 14:19:21 +0900

    SceneDelegate 제거 AppDelegate window 생성 Storyboard 제거 SnapKit, Alamofire, Moya 추가

---
+++ SeoulParking/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  SeoulParking
+//
+//  Created by byeonggeunSon on 2020/02/06.
+//  Copyright © 2020 손병근. All rights reserved.
+//
+
+import UIKit
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
